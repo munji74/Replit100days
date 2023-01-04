51f52652a95cf373386a7be576515b10a75fc36e
Munjwok James Alala <108124055+munji74@users.noreply.github.com>
2023-01-04 19:01:14 +0300

    Add files via upload

---
+++ Day 1.py
@@ -0,0 +1,9 @@
+print("""
+Munji
+4th January 2023
+I am signing up for Replit's 100 days of Python challenge!
+I will make sure to spend some time everyday coding along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE  so I can do this from my phone wherever I happen to be. No excuses for not coding from the middle of the field! 
+I am felling 😘
+You can follow my progress at replit.com/@habibmunji and github.com/munji74
+""")
