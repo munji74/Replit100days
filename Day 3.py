461f791be1b02e408ef15dc5d3659af1406d7b16
Munjwok James Alala <108124055+munji74@users.noreply.github.com>
2023-01-06 01:00:35 +0300

    Add files via upload

---
+++ Day 3.py
@@ -0,0 +1,8 @@
+food=input("Name a food: ")
+plant=input("Name a type of plant: ")
+cooking=input("Name a method of cooking: ")
+user=input("What name describes burned food: ")
+diyItem=input("Name a DIY item: ")
+
+print("MENU")
+print("Saute ", food, " with ", user, plant, " on a bed of ", diyItem)
