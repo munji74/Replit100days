8a8ca6a73a62a3a1a0d49e593018c131cf1f3571
Munjwok James Alala <108124055+munji74@users.noreply.github.com>
2023-01-07 18:17:28 +0300

    Add files via upload

---
+++ Day 4.py
@@ -0,0 +1,12 @@
+print("=== Your Adventure Si,ulator ===")
+print("""You'll be asked a bunch of questioms
+then we'll make you up an amzaing 
+story with YOU as the star! 😁""")
+print()
+name=input("Your name: ")
+enemy=input("Your worst enemy's name: ")
+superPower= input("Your superpower: ")
+print()
+print("Our story begins as our hero\033[34m",name, "\033[0mapproaches a foreboding castle...")
+print("Suddenly a bolt of \033[32mlightning struck the ground at the feet of\033[0m\033[34m",name)
+print("\033[0m'Our final battle begins!' shouts the evil\033[31m", enemy, "\033[0mclearly missing the fact that", name, "has the power of\033[33m", superPower, "\033[0m which means they'll win quite easily")
