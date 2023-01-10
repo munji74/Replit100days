5b3825387d480990bee7c75b1032d9b5dc337562
Munjwok James Alala <108124055+munji74@users.noreply.github.com>
2023-01-10 16:33:21 +0300

    Add files via upload

---
+++ Day 7 lesson.py
@@ -0,0 +1,12 @@
+tvShow = input("What is your favorite tv show? ")
+if tvShow == "Friends":
+  print("Nice 👌")
+  faveCharacter = input("Who is your favorite character? ")
+  if faveCharacter == "Joey":
+    print("Right answer, Joey is cool")
+  else:
+    print("Nah, Joey's the greatest")
+elif tvShow == "Two and a half men":
+  print("Aww, sad times")
+else:
+  print("Yeah, that's cool and all…")
---
+++ Day 7.py
@@ -0,0 +1,16 @@
+print("🧑🏿‍🌾 Fake Fan Finder 👀")
+print("-----------------")
+
+anime=input("What is your favourite anime: ")
+if anime == "One Piece":
+    character=input("Oh really?! Name any of the Characters: ")
+    if character == "Nami":
+        job=input("You got that by Pure chance. Okay then, what is her job on the ship: ")
+        if job == "Navigator":
+            bounty=input("Hmph! What was her first bounty then?: ")
+        else:
+            print("Liar")
+    else:
+        print("Aha, you are a fraud")
+else:
+    print("That is tough...")
