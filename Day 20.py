9c01b87df38cb9f7a13bdaed26d6ce2af1ce0e6d
Munjwok James Alala <108124055+munji74@users.noreply.github.com>
2023-01-23 17:08:42 +0300

    Add files via upload

---
+++ Day 20.py
@@ -0,0 +1,6 @@
+start=int(input("What number do want to start at? "))
+end=int(input("What number do u want to end at? "))
+increment=int(input("Increment between the numbers: "))
+
+for i in range(start,end,increment):
+    print(i)
